39643ca85a927bdf13194373553bbb3939ad8c38
Batch-21 Sayed <samadigithub@gmail.com>
2020-11-10 15:05:13 -0500

    committin again to see if it is tracking

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main2(String[] args) {
         //write your code here
-        System.out.println("Hello1");
+        System.out.println("Hello");
 
     }
 }
