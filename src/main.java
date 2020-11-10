405e2e2a305578eaaa81da6d6eecdeae8abb1be2
Batch-21 Sayed <samadigithub@gmail.com>
2020-11-10 15:49:27 -0500

    line one

--- src/main.java
+++ src/main.java
@@ -1,7 +1,8 @@
 public class main {
     public static void main2(String[] args) {
         //write your code here
-        System.out.println("Hello");
+        System.out.println("Hello1");
+        //line one
 
     }
 }
